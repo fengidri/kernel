1e659acd92d09580429f4ed6ad7b74a617bb9ac5
fengidri <fengidri@gmail.com>
2018-12-06 17:13:53 +0800

    fix

--- tcp_record_4.9/output.c
+++ tcp_record_4.9/output.c
@@ -34,7 +34,7 @@ static int format(struct sock *sk, char *msg, int size)
     last = last * 10;
 
     ms = val.tv_sec * 1000 + val.tv_usec / 1000;
-    ms = ms % ((1024 - 1) * 10)
+    ms = ms % ((1024 - 1) * 10);
 
 
     in_flight = tcp_packets_in_flight(tp);
